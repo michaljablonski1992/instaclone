fd80bce9ae875c8fa6fe672190c4879effc1a0bc
mjmj1992 <michal.jablonski1992@gmail.com>
2024-01-12 14:48:15 +0100

    create post modal added

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'bootstrap', to: 'bootstrap.min.js', preload: true
 pin 'popper', to: 'popper.js', preload: true
+pin 'filepond', to: 'https://ga.jspm.io/npm:filepond@4.30.4/dist/filepond.js'
+pin_all_from 'app/javascript/custom', under: 'custom'
+pin 'filepond-plugin-image-preview', to: 'https://ga.jspm.io/npm:filepond-plugin-image-preview@4.6.11/dist/filepond-plugin-image-preview.js'
