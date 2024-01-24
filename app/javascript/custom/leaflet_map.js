be9f4fdb140b2ac0dc9acaeb19f29bd3b37bf998
mjmj1992 <michal.jablonski1992@gmail.com>
2024-01-24 23:33:56 +0100

    location and ability to see on map added to posts;

--- app/javascript/custom/leaflet_map.js
+++ app/javascript/custom/leaflet_map.js
@@ -1,5 +1,6 @@
 window.photon_geocoder = L.Control.Geocoder.photon();
 window.location_geocoder = L.Control.geocoder({ collapsed: false, showUniqueResults: false, geocoder: window.photon_geocoder });
+window.map_markers = L.layerGroup()
 window.loadMap = function(container) {
   if(!document.getElementById(container).classList.contains('leaflet-container')) {
     window.location_map = L.map(container).setView([51.9189046, 19.1343786], 2); //starting position
@@ -10,6 +11,7 @@ window.loadMap = function(container) {
       crossOrigin: true
     }).addTo(location_map);
     window.location_geocoder.addTo(location_map);
+    window.map_markers.addTo(location_map);
   }
   window.location_geocoder._input.focus();
 }
