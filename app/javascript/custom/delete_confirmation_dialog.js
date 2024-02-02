cf54407d513f42939a193a866a109d8c7c189879
mjmj1992 <michal.jablonski1992@gmail.com>
2024-02-02 12:57:09 +0100

    unnecessary console.log removed

--- app/javascript/custom/delete_confirmation_dialog.js
+++ app/javascript/custom/delete_confirmation_dialog.js
@@ -1,7 +1,3 @@
-window.addEventListener(('turbo:render'), (e) => {
-  console.log(e);
-})
-
 window.addEventListener(('turbo:load'), () => {
   document.addEventListener('submit', (event) => {
     if (event.target && event.target.className === 'dbl-delete-alertbox') {
